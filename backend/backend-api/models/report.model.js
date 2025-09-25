fea38cc42853d13201401e0f4c45f763fadfbd91
Ayush A Karanth <ayushkaranth2005@gmail.com>
2025-09-25 23:24:24 +0530

    reporting

--- backend/backend-api/models/report.model.js
+++ backend/backend-api/models/report.model.js
@@ -1,3 +1,48 @@
+// import mongoose from "mongoose";
+
+// const reportSchema = new mongoose.Schema(
+//   {
+//     userId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     hazardType: {
+//       type: String,
+//       enum: ["flood", "tsunami", "oil_spill", "high_waves", "other"],
+//       required: true,
+//     },
+//     description: {
+//       type: String,
+//     },
+//     mediaUrl: {
+//       type: String, // Cloudinary URL
+//     },
+//     latitude: {
+//       type: Number,
+//       required: true,
+//     },
+//     longitude: {
+//       type: Number,
+//       required: true,
+//     },
+//     status: {
+//       type: String,
+//       enum: ["pending", "verified", "rejected"],
+//       default: "pending",
+//     },
+//     source: {
+//       type: String,
+//       enum: ["user", "reddit", "newsapi", "google_alerts","google alerts"],
+//       default: "user",
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// const Report = mongoose.model("Report", reportSchema);
+// export default Report;
+
 import mongoose from "mongoose";
 
 const reportSchema = new mongoose.Schema(
@@ -31,9 +76,13 @@ const reportSchema = new mongoose.Schema(
       enum: ["pending", "verified", "rejected"],
       default: "pending",
     },
+    source: {
+      type: String,
+      enum: ["user", "reddit", "newsapi", "google_alerts","google alerts"],
+      default: "user",
+    },
   },
   { timestamps: true }
 );
 
-const Report = mongoose.model("Report", reportSchema);
-export default Report;
+export default mongoose.models.Report || mongoose.model("Report", reportSchema);
