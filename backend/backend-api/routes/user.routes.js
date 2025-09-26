fc6b347d658ecf58b56b29eeb49637575cea0650
Ayush A Karanth <ayushkaranth2005@gmail.com>
2025-09-26 11:53:57 +0530

    user

--- backend/backend-api/routes/user.routes.js
+++ backend/backend-api/routes/user.routes.js
@@ -1,6 +1,7 @@
 
 import express from "express";
 import { getProfile, updateProfile, getAllUsers } from "../controller/user.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
